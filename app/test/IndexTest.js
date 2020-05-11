16928dccf803fd61c1515b4c19ff78cf1be80ce0
Lydia <lydiagashawtena@gmail.com>
2020-05-11 23:05:25 +0300

    again

--- app/test/IndexTest.js
+++ app/test/IndexTest.js
@@ -2,9 +2,6 @@ const expect = require('chai').expect
 const server = require('../index');
 
 describe('test', () => {
-    afterEach(async function(){
-        server.close();
-    })
     it('should return a string', () => {
         expect('ci with travis').to.equal('ci with travis');
     });
