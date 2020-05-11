2c91c9ffe42513d2f0f252f8f3325ef917eacf0c
Lydia <lydiagashawtena@gmail.com>
2020-05-11 12:07:11 +0300

    second

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('ci with travis');
+  res.send('ci with travis yay');
 });
 
 const server = app.listen(3000, () => {
